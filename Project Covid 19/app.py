dd62812daac8c68823bd379d3424dd6c8c3c3190
atharv4819 <atharvsingh7320@gmail.com>
2026-07-08 15:52:17 +0530

    Update app.py

--- Project Covid 19/app.py
+++ Project Covid 19/app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 # ---------------------------------------------------
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("model.keras")
+    return tf.keras.models.load_model("Project Covid 19/model.keras")
 
 model = load_model()
 
